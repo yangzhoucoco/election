4e4c8e6cadd5c02937330a9b87f21b1f16bcc572
Yang Zhou <yangzhou@YangdeMacBook-Air.local>
2024-03-05 22:55:02 -0500

    updata raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,25 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dataverse)
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+ces2020 <-
+  get_dataframe_by_name(
+    filename = "CES20_Common_OUTPUT_vv.csv",
+    dataset = "10.7910/DVN/E9N6PH",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) |>
+  select(votereg, CC20_410, gender, educ)
 
 
 
+
+zy01235674.
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(ces2020, "data/raw_data/ces2020.csv")
 
          
